13996d4b7cff590c48387c684e57cbfcbd4b554f
Botmakertry <168511186+Botmakertry@users.noreply.github.com>
2024-04-30 11:26:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import telebot
 ##TOKEN DETAILS
 TOKEN = "TRON"
 
-BOT_TOKEN = "5710284858:AAHcIDYAtWAC01p8BsHRl4cIwhcKpBqNlTQ"
-PAYMENT_CHANNEL = "@testpostchnl" #add payment channel here including the '@' sign
-OWNER_ID = 5151868182 #write owner's user id here.. get it from @MissRose_Bot by /id
-CHANNELS = ["@testpostchnl"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
+BOT_TOKEN = "6802368803:AAEBVTpfvbcdkddz_03PVEQUJCO-Bzw6vCU"
+PAYMENT_CHANNEL = "@gmeepayout " #add payment channel here including the '@' sign
+OWNER_ID = 5853493137 #write owner's user id here.. get it from @MissRose_Bot by /id
+CHANNELS = ["@SpotterSignal", "@AirdropSpotter"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
-Daily_bonus = 1 #Put daily bonus amount here!
-Mini_Withdraw = 0.5  #remove 0 and add the minimum withdraw u want to set
-Per_Refer = 0.0001 #add per refer bonus here
+Daily_bonus = 0.5 #Put daily bonus amount here!
+Mini_Withdraw = 30  #remove 0 and add the minimum withdraw u want to set
+Per_Refer = 5 #add per refer bonus here
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
